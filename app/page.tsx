95fe3579073969b69c32033e32129e3310571cd8
sungbongju <sungbongju1@gmail.com>
2025-12-28 22:26:16 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 "use client";
-
 import InteractiveAvatar from "@/components/InteractiveAvatar";
 
 export default function App() {
   return (
-    <main className="w-screen h-screen">
+    <main className="w-screen h-screen bg-zinc-900">
       <InteractiveAvatar />
     </main>
   );
