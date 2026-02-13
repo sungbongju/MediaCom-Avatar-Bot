2f05975674565333621f1b0565b5d4cb538e5277
sungbongju <sungbongju1@gmail.com>
2026-02-13 11:01:16 +0900

    Update constants.ts

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,23 +1,7 @@
 export const AVATARS = [
   {
-    avatar_id: "Ann_Therapist_public",
-    name: "Ann Therapist",
-  },
-  {
-    avatar_id: "Shawn_Therapist_public",
-    name: "Shawn Therapist",
-  },
-  {
-    avatar_id: "Bryan_FitnessCoach_public",
-    name: "Bryan Fitness Coach",
-  },
-  {
-    avatar_id: "Dexter_Doctor_Standing2_public",
-    name: "Dexter Doctor Standing",
-  },
-  {
-    avatar_id: "Elenora_IT_Sitting_public",
-    name: "Elenora Tech Expert",
+    avatar_id: "Wayne_20240711",
+    name: "미컴 전공 상담사",
   },
 ];
 
